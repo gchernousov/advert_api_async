60b27e759aa38d93df4f69239d5c0c0d23751f16
gchernousov <seasidesuicide@yandex.ru>
2022-12-16 21:09:21 +0500

    add test_get_user

--- app/tests/api.py
+++ app/tests/api.py
@@ -5,8 +5,8 @@ from .config import API_URL
 
 # def base_request(method: Literal["get", "post", "patch", "delete"], path: str, *args, **kwargs):
 #     print(f'>>> request to URL :: {API_URL}{path}')
-#
-#
+
+
 def get_user(user_id: int):
     url = f'{API_URL}/users/{user_id}'
     response = requests.get(url)
--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -17,17 +17,8 @@ def test_create_user():
     assert new_user['name'] == 'user_123'
 
 
-    # def test_create_user(self, root_user_token):
-    #     user_id = api.create_user("user_2", DEFAULT_PASSWORD)["id"]
-    #     user = api.get_user(user_id, root_user_token)
-    #     assert user["name"] == "user_2"
-    #
-    # def test_create_user(self):
-    #     print('>>> test create user')
-    #     result = create_user('user_1', 'psw123', 'testuser1@yandex.ru')
-    #     data = result.json()
-    #     user = get_user(data['id'])
-    #     print(f'result.id = {data["id"]}')
-    #     print(f'user.name = {user["name"]}')
-    #     assert result.status_code == 200
-    #     assert user['name'] == 'user_1'
+def test_get_user(root_user):
+    user = get_user(root_user['id'])
+    assert user == {'id': root_user['id'], 'name': root_user['name'],
+                    'email': root_user['email'], 'registration': root_user['registration_date'],
+                    'advertisement': root_user['advertisement']}
